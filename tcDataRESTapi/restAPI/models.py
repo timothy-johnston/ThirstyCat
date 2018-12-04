6970dce68756e233c8f8a2a15af11a9169c35c7f
Tim Johnston <tedwardj11@gmail.com>
2018-12-03 21:19:42 -0500

    created database, added Drinks table

--- tcDataRESTapi/restAPI/models.py
+++ tcDataRESTapi/restAPI/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+
+class Drinks(models.Model):
+    time_start = models.DateTimeField()
+    time_end = models.DateTimeField()
+    photo_path = models.CharField(max_length = 256)
+    drinks_today = models.IntegerField(default = 0)
+    def __str__(self):
+        return self.photo_path
+    def recentlyGotDrink(self):
+        return self.time_start >= timezone.now() - datetime.timedelta(days = 1)
+    
