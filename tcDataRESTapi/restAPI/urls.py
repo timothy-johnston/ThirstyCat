1f7fc628a470616a858fa9fd2f45109c4f261f97
Tim Johnston <tedwardj11@gmail.com>
2018-12-18 20:05:04 -0500

    FIX NEW HEALTH CHECK METHODS IN VIEWS.PY, started health check endpoints

--- tcDataRESTapi/restAPI/urls.py
+++ tcDataRESTapi/restAPI/urls.py
@@ -8,10 +8,11 @@ from rest_framework.views import APIView
 
 urlpatterns = [
     path('drinks/drinkstoday', views.getDrinksToday, name= 'drinkstoday'),
-    path('drinks/timeoflastdrink', views.getTimeOfLastDrink, name= 'timeoflastdrink'),
-    path('drinks/photooflastdrink', views.getPhotoOfLastDrink, name= 'photooflastdrink'),
+    #path('drinks/timeoflastdrink', views.getTimeOfLastDrink, name= 'timeoflastdrink'),
+    #path('drinks/photooflastdrink', views.getPhotoOfLastDrink, name= 'photooflastdrink'),
     path('drinks/add/<str:startTime>+<str:endTime>+<int:numDrinks>', views.addDrink),
     path('drinks/addBasic/<int:numDrinks>', views.addDrinkBasic, name = 'addDrinkBasic'),
-    #path('drinks/imageupload/(?P<filename>[^/]+$', views.FileUploadView.as_view()),
     path('drinks/imageupload/<str:filename>', views.FileUploadView),
+    path("health/ping", views.healthCheckPing),
+    path('health/status', views.healthCheckStatus),
 ]
--- tcDataRESTapi/restAPI/views.py
+++ tcDataRESTapi/restAPI/views.py
@@ -10,7 +10,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from braces.views import CsrfExemptMixin
 
-# Create your views here.
+lastPingTime = timezone.now() - timezone.now()
+
+# Create views here.
 
 def getDrinksToday(request):
     return HttpResponse(Drinks.objects.latest('id').drinksToday)
@@ -27,12 +29,32 @@ def addDrinkBasic(request, numDrinks):
     newdrink.save()
     return HttpResponse('New entry added: ' + str(newdrink.drinksToday))
 
+def healthCheckPing(request):
+    lastPingTime = timezone.now()                               #Not working currently, fix scope of lastPingTime
+    return HttpResponse(200)
+
+def healthCheckStatus(request):
+    #The piControl script should hit this endpoint once every 5 min
+    #If it hasn't, the program is down                     
+    if (timezone.now().minute - lastPingTime.minute > 5):     #Not working currently, fix scope of lastPingTime      
+        status = "down"
+    else:
+        status = "up"
+
+    return HttpResponse('Status: ' + status + '. Last update: ' + lastPingTime.hour + ':' + lastPingTime.minute + ':' + lastPingTime.second)
+
+
+#Below code is toying with making endpoints for uplaoding/retrieving images
+#Should create feature branch and remove this from master
+
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication 
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
 
     def enforce_csrf(self, request):
         return
+
+
 
 #@method_decorator(csrf_exempt, name='dispatch')
 class FileUploadView(CsrfExemptMixin, APIView):
