6dfdf889658182ebccf96c266d96c1fc7adc8d2c
Tim Johnston <tedwardj11@gmail.com>
2019-01-03 05:50:38 -0500

    configured application.properties to handle multipart file uploads

---
+++ javaRestApi/thirstycat/src/main/java/com/tj/thirstycat/ThirstycatApplication.java
@@ -0,0 +1,14 @@
+package com.tj.thirstycat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirstycatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirstycatApplication.class, args);
+	}
+
+}
+
---
+++ javaRestApi/thirstycat/src/test/java/com/tj/thirstycat/ThirstycatApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tj.thirstycat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThirstycatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
