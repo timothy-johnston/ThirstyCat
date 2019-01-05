6cdd28c8d048a548a803423dccfb875f9cb3cac4
Tim Johnston <tedwardj11@gmail.com>
2019-01-05 10:18:22 -0500

    added DrinkRepository and fixed build issues

---
+++ javaRestApi/thirstycat/src/main/java/com/tj/thirstycat/repository/DrinkRepository.java
@@ -0,0 +1,9 @@
+package com.tj.thirstycat.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import entity.Drink;
+
+public interface DrinkRepository extends CrudRepository<Drink, Long> {
+
+}
