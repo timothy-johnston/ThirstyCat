ddcc1dc1a38bd71453f92147e72cd0593c5f845a
Tim Johnston <tedwardj11@gmail.com>
2019-01-08 21:01:19 -0500

    initialized new spring boot rest api for image uploads, 1st GET request is working

---
+++ javaRestApi/thirstycat_api/src/main/java/com/tj/thirstycat_api/ThirstycatApiApplication.java
@@ -0,0 +1,14 @@
+package com.tj.thirstycat_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirstycatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirstycatApiApplication.class, args);
+	}
+
+}
+
---
+++ javaRestApi/thirstycat_api/src/main/java/com/tj/thirstycat_api/controller/ImageUploadController.java
@@ -0,0 +1,22 @@
+package com.tj.thirstycat_api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ImageUploadController {
+
+	//Upload directory
+	private String upload_dir = "/";
+	
+	//Rough health check endpoint
+	@GetMapping("/api/status")
+	@ResponseBody
+	public String getStatus() {
+		return "I live!";
+	}
+	
+	
+	
+}
