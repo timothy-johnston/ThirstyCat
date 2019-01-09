15160e6382b1ed9dc25421373602ece6dd49bb6c
Tim Johnston <tedwardj11@gmail.com>
2019-01-08 20:31:54 -0500

    still need to fix autowiring / component scanning

--- javaRestApi/thirstycat/src/main/java/com/tj/thirstycat/repository/DrinkRepository.java
+++ javaRestApi/thirstycat/src/main/java/com/tj/thirstycat/repository/DrinkRepository.java
@@ -1,9 +1,6 @@
 package com.tj.thirstycat.repository;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import entity.Drink;
