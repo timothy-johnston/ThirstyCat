68ecc63435c60c2bb6e711d1fd972d6748057cd4
Tim Johnston <tedwardj11@gmail.com>
2019-02-13 20:45:19 -0500

    made quick example controller

--- web/thirstyCatWeb/src/main/java/com/tj/thirstyCatWeb/ThirstyCatWebApplication.java
+++ web/thirstyCatWeb/src/main/java/com/tj/thirstyCatWeb/ThirstyCatWebApplication.java
@@ -2,14 +2,21 @@ package com.tj.thirstyCatWeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//SpringBootServletInitializer runs SpringApplication from a WAR deployment
 @SpringBootApplication
-public class ThirstyCatWebApplication {
+public class ThirstyCatWebApplication  extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ThirstyCatWebApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ThirstyCatWebApplication.class, args);
 		
-		System.out.println("We runnin");
 		
 	}
 
