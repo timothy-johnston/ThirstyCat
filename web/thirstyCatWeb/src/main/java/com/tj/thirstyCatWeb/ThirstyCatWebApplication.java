0ad239f50f1e7fe968d27ead2d3ed2fde7af9698
Tim Johnston <tedwardj11@gmail.com>
2019-02-13 19:17:09 -0500

    initialized spring boot project and added dependencies to pom

---
+++ web/thirstyCatWeb/src/main/java/com/tj/thirstyCatWeb/ThirstyCatWebApplication.java
@@ -0,0 +1,17 @@
+package com.tj.thirstyCatWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirstyCatWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirstyCatWebApplication.class, args);
+		
+		System.out.println("We runnin");
+		
+	}
+
+}
+
