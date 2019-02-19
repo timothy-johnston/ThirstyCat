af3ad52b003b6638e092e5a4e27736cdf6787417
Tim Johnston <tedwardj11@gmail.com>
2019-02-18 20:18:52 -0500

    implemented toggle to show/hide picture & stat divs

--- web/thirstyCatWeb/src/main/resources/static/js/main.js
+++ web/thirstyCatWeb/src/main/resources/static/js/main.js
@@ -1,9 +1,45 @@
-/**
- * Hope this works
- */
-
 $(document).ready(function() {
 	console.log("halloooo")
+	
+	$("#catStatsSection").hide();
+	
+	/* When toggle buttons are clicked, change their appearance */
+	$("#toggleButtonTC, #toggleButtonCatStats").click(function() {
+		$("#toggleButtonTC").toggleClass("buttonSelected buttonUnselected");
+		$("#toggleButtonCatStats").toggleClass("buttonSelected buttonUnselected");
+		
+		$("#thirstyCatImageSection").toggle("clip");
+		$("#catStatsSection").toggle("clip");
+	})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 })
 
 
