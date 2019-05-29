c053a53b4212bbbf31e709e04265c0d043023f20
Tim Johnston <tedwardj11@gmail.com>
2019-05-29 17:54:11 -0400

    extracted spring boot project template from initializr

---
+++ webApp/thirstyCat/src/main/java/com/tj/thirstyCat/ThirstyCatApplication.java
@@ -0,0 +1,13 @@
+package com.tj.thirstyCat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirstyCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirstyCatApplication.class, args);
+	}
+
+}
---
+++ webApp/thirstyCat/src/test/java/com/tj/thirstyCat/ThirstyCatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tj.thirstyCat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThirstyCatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
