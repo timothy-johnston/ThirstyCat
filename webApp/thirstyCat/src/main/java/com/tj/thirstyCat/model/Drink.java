f93a04064c03d2b16d97e37c2b119edc0770830f
Tim Johnston <tedwardj11@gmail.com>
2019-05-30 19:45:08 -0400

    added multipart file upload properties

--- webApp/thirstyCat/src/main/java/com/tj/thirstyCat/model/Drink.java
+++ webApp/thirstyCat/src/main/java/com/tj/thirstyCat/model/Drink.java
@@ -1,5 +1,31 @@
 package com.tj.thirstyCat.model;
 
-public class Drink {
+import java.io.File;
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "drinks")
+public class Drink extends AuditModel {
+
+	@Id
+	@GeneratedValue(generator = "drinkId_generator")
+	@SequenceGenerator(name = "drinkId_generator", sequenceName = "drinkId_dequence", initialValue = 1)
+	private Long id;
+	
+	@NotBlank
+	private Date startDate;
+	
+	@NotBlank
+	private Date endDate;
+	
+	@NotBlank
+	private File image;
+	
 }
