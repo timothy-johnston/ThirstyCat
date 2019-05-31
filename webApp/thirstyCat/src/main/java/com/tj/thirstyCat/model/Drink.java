ac5d6b5c3d2aa49f6a048795085be31a532dc619
Tim Johnston <tedwardj11@gmail.com>
2019-05-31 14:25:32 -0400

    implemented drink and image model

--- webApp/thirstyCat/src/main/java/com/tj/thirstyCat/model/Drink.java
+++ webApp/thirstyCat/src/main/java/com/tj/thirstyCat/model/Drink.java
@@ -1,5 +1,56 @@
 package com.tj.thirstyCat.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "drinks")
 public class Drink {
+	
+	@Id
+	@GeneratedValue(generator = "image_generator")
+	@SequenceGenerator(name = "image_generator", sequenceName = "image_sequence", initialValue = 1)
+	private Long id;
+	private Date startTime;
+	private Date endTime;
+	
+	public Drink() {
+		
+	}
+	
+	public Drink(Date startTime, Date endTime) {
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	
 
 }
--- webApp/thirstyCat/src/main/java/com/tj/thirstyCat/model/Image.java
+++ webApp/thirstyCat/src/main/java/com/tj/thirstyCat/model/Image.java
@@ -1,10 +1,56 @@
 package com.tj.thirstyCat.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "images")
 public class Image {
+	
+	@Id
+	@GeneratedValue(generator = "image_generator")
+	@SequenceGenerator(name = "image_generator", sequenceName = "image_sequence", initialValue = 1)
+	private Long id;
+	private Long drinkId;
+	
+	@Lob
+	private byte[] imageByteArray;
 
+	public Image() {
+		
+	}
+	
+	public Image(Long drinkId, byte[] imageByteArray) {
+		this.drinkId = drinkId;
+		this.imageByteArray = imageByteArray;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getDrinkId() {
+		return drinkId;
+	}
+
+	public void setDrinkId(Long drinkId) {
+		this.drinkId = drinkId;
+	}
+
+	public byte[] getImageByteArray() {
+		return imageByteArray;
+	}
+
+	public void setImageByteArray(byte[] imageByteArray) {
+		this.imageByteArray = imageByteArray;
+	}
+	
 }
