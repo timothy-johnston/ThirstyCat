83e95189e51194bbfa682d43159d3e214ff97dba
Tim Johnston <tedwardj11@gmail.com>
2019-05-30 20:37:25 -0400

    added test skeletons for drinkController and drinkRepository

---
+++ webApp/thirstyCat/src/test/java/com/tj/thirstyCat/controller/DrinkControllerTest.java
@@ -0,0 +1,57 @@
+package com.tj.thirstyCat.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DrinkControllerTest {
+
+	//Drink tests---------------------------------------------------------------
+	//These tests are related specifically to retrieving & persisting drink info
+	@Test
+	public void newDrinkEndpointCallsAddDrink() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void retrieveLastDrinkEndpointCallsRetriveLastDrink() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void retrieveAllDrinksEndpointCallsRetriveAllDrinks() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void retriveDrink_ID_EndpointCallsRetrieveDrink_ID() {
+		fail("Not yet implemented");
+	}
+	
+	
+	//Image tests---------------------------------------------------------------
+	//These tests are related specifically to retrieving & favoriting images
+	
+	@Test
+	public void retrieveLastImageEndpointCallsRetrieveLastImage() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void retrieveAllImagesEndpointCallsRetrieveAllImages() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void retrieveImage_ID_EndpointCallsRetrieveImage_ID() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void favoriteImage_ID_EndpointCallsFavoriteImage_ID() {
+		fail("Not yet implemented");
+	}
+	
+
+}
---
+++ webApp/thirstyCat/src/test/java/com/tj/thirstyCat/repository/DrinkRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.tj.thirstyCat.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DrinkRepositoryTest {
+
+	//Image tests-----------------------------------------------------------
+	//These tests are related specifically to retrieving & persisting drinks
+	@Test
+	public void addDrinkPersistsDrinkInfoToDatabase() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void retrieveLastDrinkRetrievesLastDrinkInfoFromDatabase() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void retrieveAllDrinksRetrievesAllDrinkInfoFromDatabase() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void retrieveDrink_ID_RetriefsDrinkWithMatchingIdInfoFromDatabase() {
+		fail("Not yet implemented");
+	}
+	
+	
+	//Image tests-----------------------------------------------------------
+	//These tests are related specifically to retrieving & favoriting images
+	
+	@Test
+	public void uploadImageUploadsImageToDatabase() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void retrieveLastImageEnRetrievesLastImageFromDatabase() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void retrieveAllImagesRetrievesAllImagesFromDatabase() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void retrieveImage_ID_RetrievesImageWithCorrectIdFromDatabase() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void favoriteImage_ID_FavoritesImageWithCorrectIdFromDatabase() {
+		fail("Not yet implemented");
+	}
+}
