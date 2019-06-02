6edccc187f44bbbed46cfa112f1a2fd54afc83d7
Tim Johnston <tedwardj11@gmail.com>
2019-06-02 10:46:59 -0400

    added scripts in /database to use as db creation template

---
+++ database/create-capstone-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#----Use below for template to create new db creation shell script----#
+# BASEDIR=$(dirname $0)
+# psql -U postgres -f "$BASEDIR/dropdb.sql" &&
+# createdb -U postgres capstone &&
+# psql -U postgres -d capstone -f "$BASEDIR/schema.sql" &&
+# psql -U postgres -d capstone -f "$BASEDIR/user.sql" &&
+# psql -U postgres -d capstone -f "$BASEDIR/data.sql"
+#---------------------------------------------------------------------#
