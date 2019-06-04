694554de9bd8800cb9e02d4f3dec7cc31542a9e6
Tim Johnston <tedwardj11@gmail.com>
2019-06-03 23:38:49 -0400

    verified get and post functionality for drinkController

--- webApp/thirstyCat/src/main/java/com/tj/thirstyCat/controller/DrinkController.java
+++ webApp/thirstyCat/src/main/java/com/tj/thirstyCat/controller/DrinkController.java
@@ -2,12 +2,18 @@ package com.tj.thirstyCat.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tj.thirstyCat.model.Drink;
@@ -18,6 +24,15 @@ public class DrinkController {
 
 	@Autowired
 	private DrinkRepository drinkRepository;
+	
+	//Returns csrf token
+	//Is this secure? TODO: Research
+	//Following https://stackoverflow.com/questions/33125598/how-to-handle-csrf-protection-with-spring-restful-web-services
+	@RequestMapping(value="/csrf-token", method=RequestMethod.GET)
+	public @ResponseBody String getCsrfToken(HttpServletRequest request) {
+	    CsrfToken token = (CsrfToken)request.getAttribute(CsrfToken.class.getName());
+	    return token.getToken();
+	}
 	
 	@GetMapping("/apiTest")
 	public String apiTest() {
