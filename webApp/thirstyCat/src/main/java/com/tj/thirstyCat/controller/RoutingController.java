c52b8cca5b36f13197ef2c99ff7f2c86c118390c
Tim Johnston <tedwardj11@gmail.com>
2019-06-18 20:54:37 -0400

    wrestled valiantly with thymeleaf until it decided to give in and serve my static content

--- webApp/thirstyCat/src/main/java/com/tj/thirstyCat/controller/RoutingController.java
+++ webApp/thirstyCat/src/main/java/com/tj/thirstyCat/controller/RoutingController.java
@@ -2,12 +2,13 @@ package com.tj.thirstyCat.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RoutingController {
 	
-	@GetMapping("/")
-	public String home() {
+	@RequestMapping("/")
+	String home() {
 		return "home";
 	}
 
