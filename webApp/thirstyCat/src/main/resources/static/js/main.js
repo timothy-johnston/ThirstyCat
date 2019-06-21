e67cfd506b4027b586188c7ae841653b01cd6b9f
Tim Johnston <tedwardj11@gmail.com>
2019-06-20 21:31:27 -0400

    implemented js function to calculate time since last drink

--- webApp/thirstyCat/src/main/resources/static/js/main.js
+++ webApp/thirstyCat/src/main/resources/static/js/main.js
@@ -1,6 +1,8 @@
 var currentDrinkId;
+var allDrinks;
 var apiURL = "http://localhost:8080";
 var apiPathLastDrink = "/lastDrink";
+var apiPathAllDrinks = "/allDrinks";
 var apiPathLastDrinkImage = "/lastImage/";
 
 $( document ).ready(function() {
@@ -8,24 +10,12 @@ $( document ).ready(function() {
 	//On initial page load, grab the most recent drink info + picture
 	initiateDrinkUpdate();
 
+	//Get all drink data
+	getAllDrinks();
+
 
 	
 })
-
-
-//AJAX call to our REST service for daily forecast
-console.log("about to make ajax call");
-function ajaxGetAllDrinks(endpoint) {
-	console.log("in ajax call");
-	return $.ajax({
-		url: endpoint,
-		type:"GET",
-		success: function(result) {
-			console.log("result is: ");
-			console.log(result);
-		}
-	});
-}
 
 //Check for new drink info/picture
 //If so, retrieve new data & update page
@@ -58,18 +48,29 @@ function getMostRecentDrinkImage(drinkInfo) {
 	});
 }
 
+function getAllDrinks() {
+	console.log("In ajax call: Get all drinks");
+	$.ajax({
+		url: apiURL + apiPathAllDrinks,
+		type: "GET",
+		success: function(result) {
+			allDrinks = result;
+		}
+	});
+}
+
 function updateDrinkInfo(drinkInfo, imageBytes) {
 	
 	//Update drink time
 	var drinkTime = formatDateToTime(new Date(drinkInfo.startTime));
 	var drinkTimeString = "Shasta took this drink at " + drinkTime + " EST.";
 	$('#drinkTime').text(drinkTimeString);
-	
-	console.log("made it down here!");
-	console.log("The drink info: ");
-	console.log(drinkInfo);
-	console.log("The image byte array: ");
-	console.log(imageBytes);
+
+	//Perform calculations for stats, charts, etc.
+	//First ensure that historical drink data has been loaded
+	console.log("Now, in updateDrinkInfo--------------");
+	console.log(allDrinks);
+	var drinkStats = calculateDrinkStats();
 }
 
 //Format time to be shown on screen
@@ -82,4 +83,49 @@ function formatDateToTime(jsDate) {
 	var timeString = ((jsDate.getHours() + 11) % 12 + 1) + ":" + jsDate.getMinutes() + amPm;
 
 	return timeString;
+}
+
+//Perform calculations on drink data
+function calculateDrinkStats() {
+
+	//Calculate drinks taken today & set html text
+	var drinksToday = getDrinksToday();
+	
+	//Calculate time since last drink;
+	var timeSinceLastDrink = getTimeSinceLastDrink();
+
+}
+
+function getDrinksToday() {
+	
+	//Current time
+	var currentDate = new Date();
+	
+	//Loop through all drink start dates; sum all drinks ocurring so far today
+	for (i = 0; i < allDrinks.length; i++) {
+		if (currentDate.getDay() == new Date(allDrinks[i].startTime).getDay()) {
+			drinksToday++;
+		}
+	}
+
+	return drinksToday;
+}
+
+function getTimeSinceLastDrink() {
+
+	elapsedMillis = allDrinks(allDrinks.length -1) - allDrinks(allDrinks.length - 2);
+
+	//Calculate hours and minutes
+	hours = Math.floor(elapsedMillis / (1000 * 60 * 60));
+	minuteRemainder = Math.round(elaspedMillis / (1000 * 60) % (Math.floor(hours) * 60));
+
+	//Format string
+	//I am nesting ternaries and I do expect to regret this in the future
+	//In fact I already regret it
+	hoursString = (hours >= 1) ? hours + ((hours > 1) ? " hours" : " hour") : "";
+	minutesString = (minues >= 1) ? minutes + " minutes" : minute + " minute";
+
+	elapsedTimeString = "It had been " + hoursString + " and " + minutesString + " since her last drink";
+
+	return elapsedTimeString;
 }
