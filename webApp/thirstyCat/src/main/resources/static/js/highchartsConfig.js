dffe3944c1c9b4e43eddb606395fbee64257e2d2
Tim Johnston <tedwardj11@gmail.com>
2019-06-22 17:30:51 -0400

    styled drinks per hour per day chart

--- webApp/thirstyCat/src/main/resources/static/js/highchartsConfig.js
+++ webApp/thirstyCat/src/main/resources/static/js/highchartsConfig.js
@@ -57,7 +57,6 @@ function createDrinkVsDay(drinkData) {
         },
         series: [{
             showInLegend: false,
-            name: 'Jane',
             data: yAxis,
             color: '#796ad1'
         }]
@@ -80,26 +79,42 @@ function createDrinkPerHourPerDay(drinkData, allDrinks) {
           type: 'heatmap',
           marginTop: 40,
           marginBottom: 80,
-          plotBorderWidth: 1
+          plotBorderWidth: 1,
+          backgroundColor: false,
         },
       
         title: {
-          text: 'Sales per employee per weekday'
+          text: 'Drinks Per 3-Hour Block of Time Each Day',
+          style: {
+        	  fontSize: "2rem"
+          } 
         },
       
         xAxis: {
-          categories: dates
+          categories: dates,
+          title: {
+              text: 'Date',
+          	  style: {
+                  fontSize: '1.5rem'
+              }
+          }
         },
       
         yAxis: {
           categories: timeBuckets,
-          title: null
+          title: {
+        	  text: 'Time Slice',
+          	  style: {
+                  fontSize: '1.5rem'
+              }
+          }
         },
-      
+        
         colorAxis: {
           min: 0,
           minColor: '#FFFFFF',
-          maxColor: '#796ad1'
+          maxColor: '#796ad1',
+          reversed: true
         },
       
         legend: {
@@ -120,9 +135,9 @@ function createDrinkPerHourPerDay(drinkData, allDrinks) {
       
         series: [{
           name: 'Sales per employee',
-          borderWidth: 1,
+          borderWidth: 6,
+          borderColor: "FFFFFF",
           data: arrayDrinkPerHourPerDay,
-        //   data: [[0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 48], [9, 4, 91]],
           dataLabels: {
             enabled: true,
             color: '#000000'
