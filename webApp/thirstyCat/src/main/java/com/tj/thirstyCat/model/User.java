f35f9fa870c260986a6a32b86bd2f9bb57d55d26
Tim Johnston <tedwardj11@gmail.com>
2019-06-25 21:50:56 -0400

    added role

--- webApp/thirstyCat/src/main/java/com/tj/thirstyCat/model/User.java
+++ webApp/thirstyCat/src/main/java/com/tj/thirstyCat/model/User.java
@@ -1,11 +1,18 @@
 package com.tj.thirstyCat.model;
 
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+import javax.persistence.JoinColumn;
 
 @Entity
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = "username"))
@@ -17,5 +24,61 @@ public class User {
 	
 	private String username;
 	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(name="user_roles", 
+		joinColumns = @JoinColumn(
+			name = "user_id", referencedColumnName = "id"),
+		inverseJoinColumns = @JoinColumn(
+			name = "role_id", referencedColumnName = "id"))
+	private Collection<Role> roles;
+	
+	public User() {
+	}
+	
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public User(String username, String password, Collection<Role> roles) {
+		this.username = username;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<Role> roles) {
+		this.roles = roles;
+	}
+	
+	
 
 }
--- webApp/thirstyCat/src/main/java/com/tj/thirstyCat/service/UserServiceImpl.java
+++ webApp/thirstyCat/src/main/java/com/tj/thirstyCat/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.tj.thirstyCat.model.Role;
 import com.tj.thirstyCat.model.User;
 import com.tj.thirstyCat.model.UserRegistration;
 import com.tj.thirstyCat.repository.UserRepository;
