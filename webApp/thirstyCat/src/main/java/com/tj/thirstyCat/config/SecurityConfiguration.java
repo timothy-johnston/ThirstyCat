6ac6c1aa6d622870f2c311f929e852c05f4ecca2
Tim Johnston <tedwardj11@gmail.com>
2019-07-08 07:32:18 -0400

    implemented remember me functionality (back end)

--- webApp/thirstyCat/src/main/java/com/tj/thirstyCat/config/SecurityConfiguration.java
+++ webApp/thirstyCat/src/main/java/com/tj/thirstyCat/config/SecurityConfiguration.java
@@ -41,7 +41,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.clearAuthentication(true)
 		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 		.logoutSuccessUrl("/login?logout")
-		.permitAll();
+		.permitAll()
+		.and()
+		.rememberMe().key("secretEnvVar");
 	}
 	
 	@Bean
